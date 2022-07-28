e56db34f18c7c347d3707b36fccaa43fbf71f791
flyingsolo-ui <lutechintesh09@gmail.com>
2022-07-28 16:42:14 +0530

    imported the files

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RxResume_PDFExport_aSwtmRO4OGapwfJfPM3Mw.pdf";
+import pdf from "../../Assets/../Assets/RxResume_PDFExport_9UwwU2RvXI3b8hzkfQTDO.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
